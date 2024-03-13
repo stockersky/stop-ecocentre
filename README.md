72ea87a58c2772d05cbd5d8219568ecf7da48ee1
stockersky <stockerskybits@gmail.com>
2024-03-13 23:55:18 +0100

    Plugin first try

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 `npx tailwindcss -i ./assets/static/input.css -o ./assets/static/output.css --watch`
 
 ## Lektor
+
+If plugin development ongoing :
+`export LEKTOR_DEV=1`
+
 `lektor serve`
 
 
--- packages/google-blogger/lektor_google_blogger.py
+++ packages/google-blogger/lektor_google_blogger.py
@@ -1,6 +1,78 @@
 # -*- coding: utf-8 -*-
 from lektor.pluginsystem import Plugin
 
+import os.path
+import os
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from icecream import ic
+from markupsafe import Markup, escape
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/blogger"]
+
+def get_blogger_articles_test():
+    return "Hello from Lektor Google Blogger !!!"
+
+def safe_html(html_doc):
+     return Markup(html_doc)
+
+def get_blogger_articles(blog_id):
+    """Shows basic usage of the People API.
+    Prints the name of the first 10 connections.
+    """
+
+    print(f">>>>>>>>>>> {os.getcwd()}<<<<<<<<<<<<<<")
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("./packages/google-blogger/token.json"):
+        creds = Credentials.from_authorized_user_file("./packages/google-blogger/token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "./packages/google-blogger/credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("./packages/google-blogger/token.json", "w") as token:
+            token.write(creds.to_json())
+
+
+    service = build("blogger", "v3", credentials=creds)
+
+    print("Call the Blogger API")
+    posts = []
+    request = service.posts().list(
+        blogId=blog_id, maxResults=20)
+
+    while request is not None:
+        response = request.execute()
+        posts.extend(response['items'])
+        request = service.posts().list_next(request, response)
+
+    print("----------------")
+    
+    
+    for post in posts:
+        # ic(post)
+        print(post['title'])
+        print(post['published'])
+        # print(f">>> {get_image_link(post['content'])}")
+        print(f"{safe_html(post['content'])}")
+        print("-----------------")
+
+    return posts
+
 
 class GoogleBloggerPlugin(Plugin):
     name = 'google-blogger'
@@ -10,3 +82,11 @@ class GoogleBloggerPlugin(Plugin):
         def test_function():
             return 'Value from plugin %s' % self.name
         context['test_function'] = test_function
+
+    def on_setup_env(self, **extra):
+        config = self.get_config()
+        blog_id = config.get('blog.blog_id', None)
+        self.env.jinja_env.globals.update(
+            get_blogger_articles_test=get_blogger_articles_test(),
+            get_blogger_articles=get_blogger_articles(blog_id)
+        )
