dd84565547377d6db7d8c91a50a4bd8e7c71b1b5
stockersky <stockerskybits@gmail.com>
2024-03-13 22:03:20 +0100

    Plugin google-blogger

---
+++ packages/google-blogger/README.md
@@ -0,0 +1,4 @@
+# google-blogger
+
+This is where a description of your plugin goes.
+Provide usage instructions here.
---
+++ packages/google-blogger/setup.py
@@ -0,0 +1,38 @@
+import ast
+import io
+import re
+
+from setuptools import setup, find_packages
+
+with io.open('README.md', 'rt', encoding="utf8") as f:
+    readme = f.read()
+
+_description_re = re.compile(r'description\s+=\s+(?P<description>.*)')
+
+with open('lektor_google_blogger.py', 'rb') as f:
+    description = str(ast.literal_eval(_description_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    author='stockersky',
+    author_email='stockerskybits@gmail.com',
+    description=description,
+    keywords='Lektor plugin',
+    license='MIT',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    name='lektor-google-blogger',
+    packages=find_packages(),
+    py_modules=['lektor_google_blogger'],
+    # url='[link to your repository]',
+    version='0.1',
+    classifiers=[
+        'Framework :: Lektor',
+        'Environment :: Plugins',
+    ],
+    entry_points={
+        'lektor.plugins': [
+            'google-blogger = lektor_google_blogger:GoogleBloggerPlugin',
+        ]
+    }
+)
